4ab16c1f0542bed23f5c1dc399121f4707207f16
huntdog1541 <gotgar1451>
2018-05-08 16:31:30 -0500

    added TestVar

---
+++ src/components/Terminal.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="terminal" ref="terminal">
+        <prompt :active="true" :prmtUsername="username" :prmtLocation="location" :prmptDirectory="directory"></prompt>
+    </div>
+</template>
+
+<script>
+    import Prompt from './prompt.vue';
+    import { store } from '../store.js'
+
+    export default {
+        name: "Terminal",
+        components: {
+            Prompt
+        },
+        store,
+        computed: {
+            username() {
+                return this.$store.state.username;
+            },
+            location() {
+                return this.$store.state.location;
+            },
+            directory() {
+                return this.$store.state.directory;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/prompt.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="prompt" v-show="active">
+        <span><span class="username">{{ username }}</span> <span class="white">@</span> <span class="location">{{ location }}</span>:<span class="directory">{{ directory }}></span> </span>
+        <input id="inputBox" v-model="command"
+               v-on:keyup="typeCommand" v-on:keyup.enter="storeCommand">
+        <div id="history"><p>{{ command }}</p></div>
+    </div>
+</template>
+
+<script>
+
+
+
+    class commands {
+
+        constructor(literal)
+        {
+            this.prompt = promptUsername + "@" +  promptLocation + promptDirectory;
+            this.literal = literal;
+        }
+
+        setResponse(response){
+            this.response = response;
+        }
+
+        setTimePullRequest(timePullRequest) {
+            this.timePullRequest = timePullRequest;
+        }
+
+
+    }
+
+    export default {
+        name: "prompt",
+
+        props: ['active', 'username', 'location', 'directory'],
+        data() {
+            return {
+
+                command: '',
+                history: [],
+                testVar: '',
+                promptUsername: username,
+                promptLocation: location,
+                promptDirectory: directory
+            }
+        },
+
+        methods: {
+            typeCommand(command) {
+
+            },
+            storeCommand() {
+                var cmds = new commands(this.command);
+                this.history.push(cmds);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .username {
+        color: #d08770;
+    }
+
+    .location {
+        color: #bf616a;
+    }
+
+    .directory {
+        color: #96b5b4;
+    }
+
+
+</style>
